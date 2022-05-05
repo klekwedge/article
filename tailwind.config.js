f8288a3f88fcd210eb784c4e87c853328cc76395
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-05 22:43:57 +0300

    feat: add section quick start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "my-dark-blue": "#141C24",
+        "my-blue": "#00C5FF",
+      },
+    },
+  },
+  plugins: [],
+};
